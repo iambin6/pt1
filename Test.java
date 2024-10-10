77461fe39de0eef76da6812bde3c2ba01c386b4c
iambin6 <miraisan2512@gmail.com>
2024-10-10 17:06:54 +0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+
+package controller;
+
+import view.Me_n_u;
+import view.Menu;
+
+import java.text.ParseException;
+
+public class Test {
+
+    public static void main(String[] args) throws ParseException {
+        CompanyManagement Menu = new CompanyManagement();
+        Menu.run();
+    }
+}
